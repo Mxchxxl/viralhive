52a743070019857c3720a52bfb72e68ef41d0a2e
Michael Emmanuel <emmanuelojonugwa009@gmail.com>
2024-07-03 04:12:14 +0100

    icons assets and home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.scss"
 
+import { Route, Routes } from "react-router-dom"
+
+import Home from './pages/Home/Home'
+
 function App()
 {
   return (
     <div className="app-container">
+      <Routes>
+        <Route path="/" element={<Home />} />
 
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,37 @@
+import "./home.scss"
+
+import Navigation from "../../components/Navigation/Navigation"
+import SocialGirl from "/images/social-girl.jpg"
+import { TiktokIcon } from "../../assets/Icons"
+
+const Home = () =>
+{
+    const heroContent = {
+        smallHeading: "Universal Influencer Outreach",
+        bigHeading: "Transform Your Brand into a Global Phenomenon.",
+        text: ( <>
+            Maximize Your Reach with Influencer Marketing. Our extensive network of TikTok, Instagram, and YouTube creators delivers highly engaging, data-backed campaigns designed to drive viral growth for your business
+        </> ),
+        buttonText: "get started",
+        image: SocialGirl,
+        imageAlt: "beautiful girl in white background",
+        socialIcon: TiktokIcon,
+        socialFollowers: "23 million",
+        socialColor: "grey"
+    }
+
+
+
+
+
+
+    return (
+        <div className="home-page">
+            <Navigation />
+
+
+        </div>
+    )
+}
+
+export default Home
