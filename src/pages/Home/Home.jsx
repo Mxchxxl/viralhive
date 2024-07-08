227739a8c8c1bde5f1b1cd4122f1182e18776a35
Michael Emmanuel <emmanuelojonugwa009@gmail.com>
2024-07-08 01:20:12 +0100

    results  component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Clients from "../../components/Clients/Clients"
 import Footer from "../../components/Footer/Footer"
 import Navigation from "../../components/Navigation/Navigation"
 import Promo from "../../components/Promo/Promo"
+import Results from "../../components/Results/Results"
 import ReuseableHero from "../../components/ReuseableHero/ReuseableHero"
 import ReviewCarousel from "../../components/ReviewCarousel/ReviewCarousel"
 import SocialGirl from "/images/social-girl.jpg"
@@ -39,6 +40,7 @@ const Home = () =>
             <ReviewCarousel />
             <Promo />
             <CTA />
+            <Results />
             <Footer />
         </div>
     )
