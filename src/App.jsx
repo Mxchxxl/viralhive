13420a3b5359c1151ca4b03db2d7e527db184e66
Michael Emmanuel <emmanuelojonugwa009@gmail.com>
2024-07-23 09:32:11 +0100

    case studies  page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss"
 
 import { Route, Routes } from "react-router-dom"
 
+import CaseStudies from "./pages/CaseStudies/CaseStudies"
 import Home from './pages/Home/Home'
 import HowItWorks from "./pages/HowItWorks/HowItWorks"
 import HowItWorksCreators from "./pages/HowItWorksCreators/HowItWorksCreators"
@@ -20,6 +21,8 @@ function App()
         <Route path="/services/youtube" element={<Youtube />} />
         <Route path="/how-it-works/for-brands" element={<HowItWorks />} />
         <Route path="/how-it-works/for-creators" element={<HowItWorksCreators />} />
+        <Route path="/results/case-studies" element={<CaseStudies />} />
+
       </Routes>
     </div>
   )
