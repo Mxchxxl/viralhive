8147ba4c0efd2b827c553c6a50703d920ee99320
Michael Emmanuel <emmanuelojonugwa009@gmail.com>
2025-03-14 05:41:49 +0100

    build 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/viralhive/',
+build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  // base: '', // Ensures relative paths for assets
 })
